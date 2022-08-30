13b2590607073b4b1f52571f37ffd82882181472
anoj-kt <anojk28@gmail.com>
2022-08-30 17:16:15 +0200

    Added background

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,14 +4,19 @@ import Col from 'react-bootstrap/Col';
 
 const Home = () => {
   return (
-    <Container>
+    <Container fluid className="home__container">
       <Row>
         <Col>
-          <div>
+          <div className="home__text-left">
             <p>What is our aim?</p>
             <p>We bring people together</p>
           </div>
-          <div>
+          
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <div className="home__text-right">
             <p>Your Service Market is a place where you can share your knowledge and skills with others.</p>
             <p>Give and Recieve Feedback to help the Community</p>
           </div>
