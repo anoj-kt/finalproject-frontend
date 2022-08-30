56d2dcd43fb618ee746c32a58500cc429b7c9058
Onur Eredo <onureredo@yahoo.com>
2022-08-30 11:56:19 +0200

    edits routes & added react-link

---
+++ src/routes/ServerRoute.js
@@ -0,0 +1,70 @@
+import { BrowserRouter as Routing, Routes, Route } from "react-router-dom";
+
+import Header from './components/Header';
+import Home from './components/Home';
+import UserLogin from './components/UserLogin';
+import Footer from "./components/Footer";
+import UserSignup from "./components/UserSignup";
+import UserProfile from "./components/UserProfile";
+import UserChangePw from "./components/UserChangePw";
+import UserChangeAvatar from "./components/UserChangeAvatar";
+import CreateAd from "./components/CreateAd";
+import UserChangeMobile from "./components/UserChangeMobile";
+import UserChangeAddress from "./components/UserChangeAddress";
+import ProvidedServiceList from "./components/ProvidedServiceList";
+import ConsumedServiceList from "./components/ConsumedServiceList";
+import ConsumedServiceDetails from "./components/ConsumedServiceDetails";
+import RequestList from "./components/RequestList";
+import RequestDetailsDone from "./components/RequestDetailsDone";
+import RequestDetailsPending from "./components/RequestDetailsPending";
+import RequestDetailsReject from "./components/RequestDetailsReject";
+import ServiceSendRequest from "./components/ServiceSendRequest";
+import ServiceSentRequest from "./components/ServiceSentRequest";
+import ErrorPage from "./components/ErrorPage";
+import EditAd from "./components/EditAd";
+import EditAdConfirm from "./components/EditAdConfirm";
+
+function ServerRoute() {
+    return (
+      <Routing>
+        <div className="Header">   
+          <Header />        
+            <div className="content">
+              <Routes>
+                {/* Single Routes */}
+                <Route exact path="/" element={<Home />}></Route>
+                <Route path="/auth/login" element={<UserLogin />}></Route>
+                <Route path="/auth/signup" element={<UserSignup />}></Route>
+                <Route path="/profile" element={<UserProfile />}></Route>
+                <Route path="/404" element={<ErrorPage />}></Route>
+  
+                {/* Routes that need id  */}
+                <Route path="/providedservices" element={<ProvidedServiceList />}></Route>          
+                <Route path="/consumedservices" element={<ConsumedServiceList />}></Route>
+                <Route path="/changepw" element={<UserChangePw />}></Route>
+                <Route path="/changeavatar" element={<UserChangeAvatar />}></Route>
+                <Route path="/changephone" element={<UserChangeMobile />}></Route>
+                <Route path="/changeaddress" element={<UserChangeAddress />}></Route>
+                <Route path="/createad" element={<CreateAd />}></Route>              
+                <Route path="/editad" element={<EditAd />}></Route> 
+                <Route path="/editad" element={<EditAdConfirm />}></Route>
+                <Route path="/requests" element={<RequestList />}></Route>
+                <Route path="/requests/details=true" element={<RequestDetailsDone />}></Route>
+                <Route path="/requests/details=pending" element={<RequestDetailsPending />}></Route>
+                <Route path="/requests/details=false" element={<RequestDetailsReject />}></Route>          
+                <Route path="/request/send" element={<ServiceSendRequest />}></Route>
+                <Route path="/request/sent" element={<ServiceSentRequest />}></Route>
+                <Route path="/service/details" element={<ConsumedServiceDetails />}></Route>
+              </Routes>
+            </div>
+          <Footer />
+        </div>
+      </Routing>
+    );
+  }
+  
+  export default ServerRoute;
+
+
+
+
