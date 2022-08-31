01f8cbd16eabc549e3143adfcc643764d7bc5be6
anoj-kt <anojk28@gmail.com>
2022-08-31 10:32:37 +0200

    necessary imports

---
+++ src/components/UserSignup.js
@@ -0,0 +1,5 @@
+import { Container, Row, Col } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import { Form, FormGroup, Label, Input } from 'reactstrap'
+import PhoneInput from 'react-phone-input-2'
+import 'react-phone-input-2/lib/style.css'
