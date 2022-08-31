fe72adf3d5d0008cfcbc868e9da2df961e651f22
anoj-kt <anojk28@gmail.com>
2022-08-31 16:14:23 +0200

    Adjusted background issue

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import './index.css';
 import Header from './components/Header';
 import Home from './components/Home';
