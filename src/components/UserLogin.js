d9fa6e0bf71483d3945cb35c417063ba0c60c65c
anoj-kt <anojk28@gmail.com>
2022-08-31 16:44:29 +0200

    Initial form set up

--- src/components/UserLogin.js
+++ src/components/UserLogin.js
@@ -12,17 +12,61 @@ const UserLogin = () => {
   const [user, setUser] = useState(initialstate);
 
   return (
-    <div>
-      <h2>USER LOGIN</h2>
-      <div>
-      <h3>Don't you have an account yet?</h3> 
-      <button>
-        <Link to="/auth/signup">Sign up here!</Link> 
-      </button>       
-      </div>
-      
-    </div>
+    <Container className="signup__container">
+    <Row>
+      <Col lg={{ span: 6, offset: 3 }}>
+        <h4>Log in</h4>
+        <Form>
+          <FormGroup>
+            <Label for="email">
+              Email
+            </Label>
+            <Input
+              id="email"
+              name="email"
+              placeholder=""
+              type="email"
+              required
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label for="password">
+              Password
+            </Label>
+            <Input
+              id="password"
+              name="password"
+              placeholder=""
+              type="password"
+              required
+            />
+            <FormFeedback invalid>
+              Passwords need to match!
+            </FormFeedback>
+          </FormGroup>
+          <div className="signup__submit">
+            <button
+            className="signup__submit-btn button"
+            >
+              Log in
+            </button>
+          </div>
+        </Form>
+      </Col>
+    </Row>
+  </Container>
   )
 }
 
 export default UserLogin
+
+// <div>
+//       <h2>USER LOGIN</h2>
+//       <div>
+//       <h3>Don't you have an account yet?</h3> 
+//       <button>
+//         <Link to="/auth/signup">Sign up here!</Link> 
+//       </button>       
+//       </div>
+      
+//     </div>
