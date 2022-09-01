b5a76538b241f49609da552dc635b23fa0a16537
anoj-kt <anojk28@gmail.com>
2022-09-01 14:48:15 +0200

    necessary imports

--- src/components/CreateAd.js
+++ src/components/CreateAd.js
@@ -1,4 +1,7 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { Container, Row, Col } from 'react-bootstrap';
+import { Form, FormGroup, Label, Input, FormFeedback } from 'reactstrap'
 
 const CreateAd = () => {
   return (
