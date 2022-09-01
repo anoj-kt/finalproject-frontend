f152ba5acd654cc2af20f385ecd4310137c295dd
anoj-kt <anojk28@gmail.com>
2022-09-01 14:52:02 +0200

    Basic bootstrap setup

--- src/components/CreateAd.js
+++ src/components/CreateAd.js
@@ -5,9 +5,49 @@ import { Form, FormGroup, Label, Input, FormFeedback } from 'reactstrap'
 
 const CreateAd = () => {
   return (
-    <div>
-      <h2>CREATE AD</h2>
-    </div>
+    <Container>
+      <Row className="justify-content-center">
+        <Col lg={{ span: 6}}>
+        <h4>Create your advertisement</h4>
+        <Form>
+          <FormGroup>
+            <Label for="email">
+              Email
+            </Label>
+            <Input
+              id="email"
+              name="email"
+              placeholder=""
+              type="email"
+              required
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label for="password">
+              Password
+            </Label>
+            <Input
+              id="password"
+              name="password"
+              placeholder=""
+              type="password"
+              required
+            />
+            <FormFeedback invalid>
+              Passwords need to match!
+            </FormFeedback>
+          </FormGroup>
+          <div className="signup__submit">
+            <button
+            className="signup__submit-btn button"
+            >
+              Create Ad
+            </button>
+          </div>
+        </Form>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
