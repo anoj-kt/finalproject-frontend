44f8d2219c39824ae9f5d294ebd43ba2f6db0ca9
anoj-kt <anojk28@gmail.com>
2022-09-05 00:08:49 +0200

    Added send request button

--- src/components/ServiceDetails.js
+++ src/components/ServiceDetails.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import { Form, FormGroup, Label, Input, FormFeedback } from 'reactstrap'
+import { Link } from 'react-router-dom';
 
 const ServiceDetails = () => {
 
@@ -44,6 +45,11 @@ const ServiceDetails = () => {
                             </div>
                         </div>
                     </div>
+                    <Link to="./service/:serviceID/request/new'" style={{textDecoration: 'none'}}>
+                        <button className="button service__request-btn">
+                            Send request
+                        </button>
+                    </Link>
                 </Col>
             </Row>
             {/* ======PRICE====== */}
