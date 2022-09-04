c24f573e841cd79f1637fbe39b189ee259c0607f
anoj-kt <anojk28@gmail.com>
2022-09-05 00:16:52 +0200

    Added basic carousel template

--- src/components/ServiceDetails.js
+++ src/components/ServiceDetails.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
+import Carousel from 'react-bootstrap/Carousel';
 import { Form, FormGroup, Label, Input, FormFeedback } from 'reactstrap'
 import { Link } from 'react-router-dom';
 
@@ -22,9 +23,49 @@ const ServiceDetails = () => {
             </Row>
             {/* ======IMAGES, DETAILS AND SEND REQUEST BUTTON====== */}
             <Row>
+                {/* ======IMAGES CAROUSEL====== */}
                 <Col>
-                    <div>IMAGE</div>
+                    <Carousel fade>
+                        <Carousel.Item>
+                            <img
+                            className="d-block w-100"
+                            src="holder.js/800x400?text=First slide&bg=373940"
+                            alt="First slide"
+                            />
+                            <Carousel.Caption>
+                            <h3>First slide label</h3>
+                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                            className="d-block w-100"
+                            src="holder.js/800x400?text=Second slide&bg=282c34"
+                            alt="Second slide"
+                            />
+
+                            <Carousel.Caption>
+                            <h3>Second slide label</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                            className="d-block w-100"
+                            src="holder.js/800x400?text=Third slide&bg=20232a"
+                            alt="Third slide"
+                            />
+
+                            <Carousel.Caption>
+                            <h3>Third slide label</h3>
+                            <p>
+                                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                            </p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                    </Carousel>
                 </Col>
+                {/* ======DETAILS AND SEND REQ====== */}
                 <Col>
                     <div className="service__details">
                         <h5 style={{borderBottom: "1px solid var(--main-grey)"}}>Details</h5>
