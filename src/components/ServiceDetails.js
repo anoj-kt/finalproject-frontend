7516952ff98fc616c983dcdfc47fe2ef0ee757d0
anoj-kt <anojk28@gmail.com>
2022-09-05 00:30:42 +0200

    Added border radius to carousel images

--- src/components/ServiceDetails.js
+++ src/components/ServiceDetails.js
@@ -25,43 +25,34 @@ const ServiceDetails = () => {
             <Row>
                 {/* ======IMAGES CAROUSEL====== */}
                 <Col>
-                    <Carousel fade>
+                    <Carousel fade className="service__carousel">
                         <Carousel.Item>
                             <img
-                            className="d-block w-100"
-                            src="holder.js/800x400?text=First slide&bg=373940"
+                            className="d-block w-100 service__carousel-image"
+                            src="http://placeimg.com/640/360/any"
                             alt="First slide"
                             />
-                            <Carousel.Caption>
-                            <h3>First slide label</h3>
-                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                            </Carousel.Caption>
                         </Carousel.Item>
                         <Carousel.Item>
                             <img
-                            className="d-block w-100"
-                            src="holder.js/800x400?text=Second slide&bg=282c34"
+                            className="d-block w-100 service__carousel-image"
+                            src="https://www.placecage.com/640/360"
                             alt="Second slide"
                             />
-
-                            <Carousel.Caption>
-                            <h3>Second slide label</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                            </Carousel.Caption>
                         </Carousel.Item>
                         <Carousel.Item>
                             <img
-                            className="d-block w-100"
-                            src="holder.js/800x400?text=Third slide&bg=20232a"
+                            className="d-block w-100 service__carousel-image"
+                            src="https://picsum.photos/640/360"
                             alt="Third slide"
                             />
-
-                            <Carousel.Caption>
-                            <h3>Third slide label</h3>
-                            <p>
-                                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-                            </p>
-                            </Carousel.Caption>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                            className="d-block w-100 service__carousel-image"
+                            src="https://placebear.com/640/360"
+                            alt="Third slide"
+                            />
                         </Carousel.Item>
                     </Carousel>
                 </Col>
